e8b38e4975a8e16c45a0fb2d56c72e6b847a8205
James Harris <james@rancher.com>
2015-01-15 17:45:39 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This will be your repo's readme
---
+++ runReorder.sh
@@ -0,0 +1,1 @@
+node reorderFormatterXml.js /Users/wizardofmath/Desktop/Rancher_Formatter_ChkStyle.xml /Users/wizardofmath/Desktop/Rancher_Fromatter.xml
