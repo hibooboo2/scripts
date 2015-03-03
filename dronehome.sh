b40235609859cf631003a2b00e2277ec394fd71a
James Harris <james@rancher.com>
2015-03-03 08:16:28 -0700

    Add drone-wall

--- dronehome.sh
+++ dronehome.sh
@@ -12,3 +12,14 @@ docker run -d --privileged \
 -v ${HOME}/drone.sqlite:/var/lib/drone/drone.sqlite \
 -v /var/run/docker.sock:/var/run/docker.sock \
 drone/drone
+
+docker stop drone-wall
+docker rm drone-wall
+docker pull scottwferg/drone-wall
+docker run -p 3000:3000 \
+-e API_SCHEME=$API_SCHEME \
+-e API_DOMAIN=$API_DOMAIN \
+-e API_TOKEN=$API_TOKEN \
+-e API_PORT=$API_PORT \
+--name="drone-wall" \
+scottwferg/drone-wall
