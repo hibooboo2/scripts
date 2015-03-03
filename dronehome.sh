f578d26b1ea772fa97b833f176b713248fb5eddc
James Harris <james@rancher.com>
2015-03-02 17:32:54 -0700

    Better drone. Keep database

--- dronehome.sh
+++ dronehome.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
-docker run -d --privileged -e DRONE_GITHUB_CLIENT=${DRONE_GITHUB_CLIENT} \
+docker rm -f drone-ci
+docker run -d --privileged \
+--name="drone-ci" \
+-e DRONE_GITHUB_CLIENT=${DRONE_GITHUB_CLIENT} \
 -e DRONE_GITHUB_SECRET=${DRONE_GITHUB_SECRET} \
--p 8080:80 -v /var/lib/drone/ -v /var/run/docker.sock:/var/run/docker.sock drone/drone
+-e DRONE_SERVER_PORT=0.0.0.0:8080 \
+-e DRONE_DATABASE_DATASOURCE=/var/lib/drone/drone.sqlite \
+-p 8080:80 \
+-v /var/lib/drone/ \
+-v ${HOME}/drone.sqlite:/var/lib/drone/drone.sqlite \
+-v /var/run/docker.sock:/var/run/docker.sock \
+drone/drone
