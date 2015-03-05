2a41b020577e349825fd12e52e7eb7da916eca9b
James Harris <james@rancher.com>
2015-03-05 09:29:15 -0700

    Add to profile

--- addScripts.sh
+++ addScripts.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+set -ex
+
 : ${MYSCRPITS:=${HOME}/scripts}
 
-[[ ! -z $(which git) ]] && git clone https://github.com/hibooboo2/scripts.git ${MYSCRPITS}
+[[ ! -d "${MYSCRPITS}" ]] && [[ ! -z $(which git) ]] && git clone https://github.com/hibooboo2/scripts.git ${MYSCRPITS}
+
+echo . ${MYSCRPITS}/.profile >> ~/.profile
+
