15a36a027892353e075f5d25c95ee75c2bc0fc53
James Harris <james@rancher.com>
2015-03-05 14:44:47 -0700

    Put certs in right place.

--- sites.sh
+++ sites.sh
@@ -2,14 +2,15 @@
 
 set -e
 
-: ${CERTS:="${HOME}/.certs"}
+: ${CERTS:=~/.certs}
+ 
+ [[ ! -d "${CERTS}" ]] && mkdir ${CERTS}
 
-[[ ! -d "${CERTS}" ]] && mkdir ${CERTS}
 docker stop nginx-proxy 2>/dev/null | echo Proxy stopped.
 docker rm -fv nginx-proxy 2>/dev/null | echo Proxy removed.
 docker run -d -p 80:80 \
  -p 443:443 \
- -v ${CERTS}/certs:/etc/nginx/certs:ro \
+ -v ${CERTS}:/etc/nginx/certs:ro \
  -v /var/run/docker.sock:/tmp/docker.sock \
  --name=nginx-proxy \
  jwilder/nginx-proxy
