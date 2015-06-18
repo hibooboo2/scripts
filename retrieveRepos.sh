c3f4bec6c610473852dc942677c0f3267dcc6a90
James Harris <james@rancher.com>
2015-06-18 11:58:39 -0700

    Much better retrieve repos script. Only clone source repos. Unless -f is passed.

--- retrieveRepos.sh
+++ retrieveRepos.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+echo ${GITHUB_AUTHTOKEN}
+function github(){
+    if [ -z "${GITHUB_AUTHTOKEN}" ];
+    then
+        curl -s https://api.github.com/${1}
+    else
+        curl -s -H "Authorization: token ${GITHUB_AUTHTOKEN}" https://api.github.com/${1}
+    fi
+}
+
+function clone() {
+    echo Cloning https://github.com/${1}/${2}
+    pushd ${CODE_HOME}/${1}/
+    hub clone ${1}/${2} > /dev/null
+    echo Cloned ${1}/${2} to ${CODE_HOME}/${1}/${2}
+    popd
+}
+
 set -e
 
 [[ -z "${1}" ]] && echo Please specify User or Org to clone. && exit 1
@@ -9,25 +27,34 @@ CMDS="hub jq curl git"
 
 for i in $CMDS
 do
-        # command -v will return >0 when the $i is not found
-	which $i >/dev/null && continue || { echo "$i command not found."; exit 1; }
+    # command -v will return >0 when the $i is not found
+    which $i >/dev/null && continue || { echo "$i command not found."; exit 1; }
 done
 
 [[ ! -d ${CODE_HOME}/ ]] && echo Making code Folder && mkdir ${CODE_HOME}/
 [[ ! -d ${CODE_HOME}/$1 ]] && echo Making User Folder Git repos && mkdir ${CODE_HOME}/$1
-[[ ! $(curl -# https://api.github.com/orgs/$1/repos | jq -r .message) == "Not Found" ]] && type=org
-[[ ! $(curl -# https://api.github.com/users/$1/repos | jq -r .message) == "Not Found" ]] && type=user
-echo Fetching ${type} ${1}
+[[ ! $(github orgs/$1/repos | jq -r .message) == "Not Found" ]] && type=org
+[[ ! $(github users/$1/repos | jq -r .message) == "Not Found" ]] && type=user
 [[ -z "${type}" ]] && echo Invalid User or Org && exit 2
-
-for i in $(curl -# https://api.github.com/${type}s/${1}/repos | jq -r .[].name)
+echo Fetching ${type} ${1}
+for i in $(github ${type}s/${1}/repos | jq -r .[].name)
 do
-    if [ ! -d ${CODE_HOME}/$1/$i ]; then
-        cd ${CODE_HOME}/${1}/
-        hub clone ${1}/${i} >/dev/null && echo Cloned ${1}/${i} to ${CODE_HOME}/${1}/${i} &
+    if [ ! -d ${CODE_HOME}/$1/$i ];
+    then
+        if [ "$(github repos/${1}/${i} | jq .parent)" == "null" ];
+        then
+            clone ${1} ${i} &
+        else
+            if [ "${2}" == "-f" ];
+            then
+                clone ${1} ${i} &
+            else
+                echo https://github.com/${1}/${i} is a Fork of Repo. Not Cloning.
+            fi
+        fi
     else
         echo You already have ${1}/$i
-        if [ "$2" == "update" ]; then
+        if [ "$2" == "-u" ]; then
             cd ${CODE_HOME}/${1}/${i}
             git fetch --all &
         fi
