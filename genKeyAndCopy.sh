3bf31724e80f95215bc9bfee259558c99772d0ca
James Jeffrey <james@rancher.com>
2015-10-29 15:01:22 -0700

    Update genKey. And git stuff

--- genKeyAndCopy.sh
+++ genKeyAndCopy.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+[[ -z "${1}" ]] && echo Must provide a host name. && read HOST
+[[ -z "${EMAIL}" ]] && echo Must provide email address. && read EMAIL
 [[ -d "~/.ssh" ]] && mkdir ~/.ssh && chmod 700 ~/.ssh && echo Made .ssh/ folder.
-[[ -f "~/.ssh/id_rsa" ]] && [[ -f "~/.ssh/id_rsa.pub" ]] && ssh-keygen -t rsa -b 4096
-ssh-copy-id ${1} && echo You can now ssh ${1} without a password.
+[[ -f "~/.ssh/id_rsa" ]] && [[ -f "~/.ssh/id_rsa.pub" ]] && ssh-keygen -b 2048 -t rsa -q -N "" -C ${EMAIL}
+[[ -f "~/.ssh/id_rsa" ]] && echo no id_rsa && exit 2
+[[ -f "~/.ssh/id_rsa.pub" ]] && echo no id_rsa.pub && exit 3
+if [[ -z "${1}" ]]
+then
+    ssh-copy-id ${HOST} && echo You can now ssh ${HOST} without a password.
+else
+    ssh-copy-id ${1} && echo You can now ssh ${1} without a password.
+fi
