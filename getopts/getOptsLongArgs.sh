0c3a474168deaf183c7e21774f05c32b0b1ea028
James Jeffrey <james@rancher.com>
2015-11-09 23:41:10 -0700

    Update getOpts

--- getopts/getOptsLongArgs.sh
+++ getopts/getOptsLongArgs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 : ${SHORT_OPTS:="ab:c-:"} # The -: allows long flags ex --this-is-a-flag --thisflag --flag
 : ${LONG_OPTS:="flag;arg:;flag-long;flag-longer-arg:;"}
-
+set -x
 is_valid_long_flag(){
     if [[ ${LONG_OPTS} == *${1}\;* || ${LONG_OPTS} == *${1}:\;* ]]
     then
@@ -23,8 +23,13 @@ need_arg(){
 
 long_args(){
     #ADD support for long flags.
+    #To use just call this function at the begining of the use of getopts before your case statement.
+    #Be sure to pass in the value of ${!OPTIND} ex: long_args "${!OPTIND}"
+    #Not sure why but cannot call ${!OPTIND} from within the function and get correct value so Must do It before hand
+    #and pass in as a var or arg. So I chose arg.
     if [ "$opt" == "-" ]
     then
+        echo  $(tput setaf 3) OPTARG: $OPTARG$(tput sgr0)
         opt=$OPTARG
         FLAG_TYPE="--"
 
@@ -56,7 +61,7 @@ long_args(){
             if [ "${NEED_ARG}" == "true" ]
             then
 
-                OPTARG="${!OPTIND}"
+                OPTARG="${1}"
                 [[ -z ${OPTARG} ]] && echo $(tput setaf 1) --${opt} requires an Argument. $(tput sgr0) && exit 1
                 [[ ${OPTARG} = -* ]] && echo $(tput setaf 1) --${opt} requires an Argument. $(tput sgr0) && exit 1
                 OPTIND=$(( $OPTIND + 1 ))
@@ -74,11 +79,14 @@ long_args(){
     fi
     #End support for long flags. now opt can use cases that are words.
     #You can use FLAG_TYPE to determine weather the called flag was long or short. - is short -- is long.
-    echo ${FLAG_TYPE}${opt} ${OPTARG}
+    # echo ${FLAG_TYPE}${opt} ${OPTARG}
+    #Uncomment above line to display flag with arg that was just parsed.
 }
 
 while  getopts "$SHORT_OPTS" opt; do
-    long_args
+
+    long_args "${!OPTIND}"
+
     case ${opt} in
         \?)
             echo $(tput setaf 1) Invalid Flag $(tput sgr0)
