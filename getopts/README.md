e2bfac4ed062364fa92c9480716f1ddfa465bedc
James Jeffrey <james@rancher.com>
2015-11-15 13:23:31 -0700

    Move getopts to another repo.

---
+++ getopts/README.md
@@ -0,0 +1,2 @@
+The purpose of this repos is to add long-arg parsing functionality to getopts in bash.
+See 
--- getopts/getOptsLongArgs.sh
+++ getopts/getOptsLongArgs.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 : ${SHORT_OPTS:="h-:"} # The -: allows long flags ex --this-is-a-flag --thisflag --flag
-: ${LONG_OPTS:="[flag]:[arg]:[flag-long][flag-longer-arg]:"}
+: ${LONG_OPTS:="[flag]:[arg]:[flag-long][flag-longer-arg]:[help]"}
 
 set -x
+source $(dirname ${BASH_SOURCE[0]})/long_args
+
 show_usage() {
             cat << EOF
 This is a demonstration script showing how to use the import of long_args support for bash getopts.
@@ -40,56 +42,6 @@ between it and the flag.
 EOF
 }
 
-long_args(){
-    #ADD support for long flags.
-    #To use just call this function at the begining of the use of getopts before your case statement.
-    #Be sure to pass in the value of ${!OPTIND} ex: long_args "${!OPTIND}"
-    #Not sure why but cannot call ${!OPTIND} from within the function and get correct value so Must do It before hand
-    #and pass in as a var or arg. So I chose arg.
-    if [ "$opt" == "-" ]
-    then
-        #Uncomment line to print current flag before processing
-        #echo  $(tput setaf 3) OPTARG: $OPTARG$(tput sgr0)
-        opt=$OPTARG
-        FLAG_TYPE="--"
-        echo OPT:${opt}
-        echo 1:${1}
-        if [[ ${opt} = *=* ]]
-        then
-            FLAG=$(echo ${opt} |cut -f 1 -d "=")
-
-            [[ ${LONG_OPTS} != *\[${FLAG}\]* ]] && echo "$(tput setaf 1) Flag: --${FLAG} is not a valid flag $(tput sgr0)" && exit 1
-            [[ ${LONG_OPTS} != *\[${FLAG}\]:* ]] && echo "$(tput setaf 1) Flag: --${FLAG} doesn\'t take an argument $(tput sgr0)" && exit 1
-
-            val=${opt#*=}
-            opt=${opt%=${val}}
-            OPTARG=${val}
-
-        elif [[ ${LONG_OPTS} = *\[${opt}\]:* ]]
-        then
-            OPTARG="${1}"
-            [[ -z ${OPTARG} ]] && echo $(tput setaf 1) --${opt} requires an Argument. $(tput sgr0) && exit 1
-            [[ ${OPTARG} = -* ]] && echo $(tput setaf 1) --${opt} requires an Argument. $(tput sgr0) && exit 1
-            OPTIND=$(( $OPTIND + 1 ))
-
-        elif  [[ ${LONG_OPTS} = *\[${opt}\]* ]]
-        then
-            OPTARG=
-
-        else
-            echo $(tput setaf 1) Flag: --${opt} is not a valid flag $(tput sgr0)
-            exit 1
-        fi
-        #End support for long flags. now opt can use cases that are words.
-        #You can use FLAG_TYPE to determine weather the called flag was long or short. - is short -- is long.
-        #Uncomment below line to display flag with arg that was just parsed.
-        # echo ${FLAG_TYPE}${opt} ${OPTARG}
-    else
-        FLAG_TYPE="-"
-    fi
-}
-[[ -z "${@}" ]] && export long_args && set +x
-
 while  getopts "$SHORT_OPTS" opt; do
     [[ -z "${@}" ]] && break
     long_args "${!OPTIND}"
@@ -99,7 +51,7 @@ while  getopts "$SHORT_OPTS" opt; do
             echo $(tput setaf 1) Invalid Flag $(tput sgr0)
             exit 1
             ;;
-        h) show_usage;;
+        h | help) show_usage;;
 
         :)
             echo What happened ${FLAG_TYPE}${opt}${OPTARG}
@@ -109,3 +61,5 @@ while  getopts "$SHORT_OPTS" opt; do
             ;;
     esac
 done
+[[ -z "${@}" ]] && show_usage
+exit 0
