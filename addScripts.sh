0ebf814bee324ee6fc1529bfaf4f852c6f78d2b5
James Jeffrey <wizardofmath@gmail.com>
2017-03-02 19:44:26 -0700

    Update typos

--- addScripts.sh
+++ addScripts.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-: ${MYSCRPITS:=${HOME}/scripts}
+: ${MYSCRIPTS:=${HOME}/scripts}
 
 function ifInstall() {
     if [ ! -z "$(which apt-get)" ]
@@ -20,23 +20,23 @@ ifInstall git
 
 [[ -z $(which git) ]] && echo You need git installed! && exit 1
 
-if [ -d "${MYSCRPITS}" ]
+if [ -d "${MYSCRIPTS}" ]
 then
-    pushd ${MYSCRPITS}
+    pushd ${MYSCRIPTS}
     git pull -r origin master
     popd
 else
-    git clone --recursive https://github.com/hibooboo2/scripts.git ${MYSCRPITS}
+    git clone --recursive https://github.com/hibooboo2/scripts.git ${MYSCRIPTS}
 fi
 
 if [ ! -f "~/.bashrc" ]
 then
-    echo . ${MYSCRPITS}/.profile >> ~/.bashrc
+    echo . ${MYSCRIPTS}/.profile >> ~/.bashrc
 elif [ ! -f "~/.profile" ]
 then
-    echo . ${MYSCRPITS}/.profile >> ~/.profile
+    echo . ${MYSCRIPTS}/.profile >> ~/.profile
 else
     echo Where do you want to source ${MYSCRIPTS}/.profile ?
     read TOSOURCE
-    [[ -f "${TOSOURCE}" ]] && echo . ${MYSCRPITS}/.profile >> ${TOSOURCE}
+    [[ -f "${TOSOURCE}" ]] && echo . ${MYSCRIPTS}/.profile >> ${TOSOURCE}
 fi
